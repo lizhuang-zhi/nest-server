64777b9eb3f1c7b648df2cd331cee9f4af4b6a4b
Mr.KLeo <59167582+lizhuang-zhi@users.noreply.github.com>
2023-04-21 18:01:48 +0800

    update

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
 @Module({
+  // 导入数据库
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
